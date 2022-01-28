dafaf3e82233ccfab3bd35536faced12569933c0
arl009 <arl009@ucsd.edu>
2022-01-27 16:34:10 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -19,4 +19,16 @@ public class MarkdownParseTest {
 	    String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
+    @Test
+    public void testmd2()throws IOException{
+		Path fileName = Path.of("test2.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of("link1.html"), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testmd3()throws IOException{
+		Path fileName = Path.of("test3.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
 }
